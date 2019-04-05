5487fd0b89d92c722d8da47a2af0249022b75786
ylu1100 <43207435+ylu1100@users.noreply.github.com>
2019-04-05 08:28:48 -0400

    Update Digits.java

--- src/Digits.java
+++ src/Digits.java
@@ -10,11 +10,25 @@ public class Digits
     //represents num
     public Digits (int num)
     {
-        //implemented in part (a)
+        int Digit=num;
+        for(int x = 10;x<Digit*10;x=x*10){
+            digitList.add(0,Digit%10);
+            Digit=Digit/10;
+        }
+//implemented in part (a)
     }
     public boolean isStrictlyIncreasing()
     {
-        //implemented in part (b)
+
+        for(int x = 1; x<digitList.size();x++)
+        {
+            if(digitList.get(x)<digitList.get(x-1))
+            {
+                return false;
+            }
+        }
+        return true;
+//implemented in part (b)
     }
     //Extra method to make the runner easier to read
     public ArrayList<Integer> getDigitList()
